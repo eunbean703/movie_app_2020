6b0f17ff9e0517b528b48bf0af0f1b440d3f5442
eunbean703 <binlim703@nate.com>
2020-10-07 05:58:16 +0900

    3. Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
-function Food({fav}) {
-  return <h1>I like {fav}</h1>;
+function Food({name , picture}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src = {picture}/>
+  </div>
 }
+
+const foodILike = [
+  {
+  name: "Kimchi",
+  image: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Korean_cuisine-Kimchi-08.jpg"
+  },
+  {
+  name: "bulgogi",
+  image: "https://www.foxyfolksy.com/wp-content/uploads/2015/08/beef-bulgogi.jpg"
+  },
+  {
+  name: "kimbap",
+  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Korean.food-Kimbap-03.jpg/1280px-Korean.food-Kimbap-03.jpg"
+  },
+  {
+  name: "samgyetang",
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Samgyetang.jpg"
+  }
+];
 
 function App() {
   return (
   <div>
-    <h1>Hello!</h1>
-    <Food fav= "kimchi" />
-    <Food fav= "ramen" />
-    <Food fav= "samgiopsal" />
-    <Food fav= "chukumi" />
+      {foodILike.map(dish => (
+      <Food name = {dish.name} picture = {dish.image} />
+      ))}
     </div>
   );
 }
