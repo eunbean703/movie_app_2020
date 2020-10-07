4e7e6314c7bfddf328acb566bdeb6eaa1f9ab249
eunbean703 <binlim703@nate.com>
2020-10-08 00:09:46 +0900

    component life Cycle

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
 import React from 'react';
 
-function Food({name , picture}) {
-  return <div>
-    <h2>I like {name}</h2>
-    <img src = {picture}/>
-  </div>
-}
-
-const foodILike = [
-  {
-  name: "Kimchi",
-  image: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Korean_cuisine-Kimchi-08.jpg"
-  },
-  {
-  name: "bulgogi",
-  image: "https://www.foxyfolksy.com/wp-content/uploads/2015/08/beef-bulgogi.jpg"
-  },
-  {
-  name: "kimbap",
-  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Korean.food-Kimbap-03.jpg/1280px-Korean.food-Kimbap-03.jpg"
-  },
-  {
-  name: "samgyetang",
-  image: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Samgyetang.jpg"
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    console.log("hello");
   }
-];
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({count: current.count +1}));
+  };
+  minus = () => {
+    this.setState(current =>({count: this.state.count-1}));
+  };
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("i just upadate");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, creul world");
+  }
+      render(){
+        console.log("im rendering");
+      return (
+      <div> 
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick = {this.add}>Add</button>
+        <button onClick = {this.minus}>Minus</button>
+        </div>
+      );
+    }
 
-function App() {
-  return (
-  <div>
-      {foodILike.map(dish => (
-      <Food name = {dish.name} picture = {dish.image} />
-      ))}
-    </div>
-  );
 }
+
 
 export default App;
